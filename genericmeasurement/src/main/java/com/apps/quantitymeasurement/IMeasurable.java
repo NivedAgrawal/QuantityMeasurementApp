b525cee862a45e392c304541cfd23b501c3337ec
NivedAgrawal <nivedagrawal0246@gmail.com>
2026-03-13 16:31:32 +0530

    Add.Added quantity measurement app usecase 15

---
+++ genericmeasurement/src/main/java/com/apps/quantitymeasurement/IMeasurable.java
@@ -0,0 +1,27 @@
+package com.apps.quantitymeasurement;
+
+public interface IMeasurable {
+
+    double getConversionFactor();
+
+    default double convertToBaseUnit(double value) {
+        return value * getConversionFactor();
+    }
+
+    default double convertFromBaseUnit(double baseValue) {
+        return baseValue / getConversionFactor();
+    }
+
+    String getUnitName();
+
+    // Default lambda for arithmetic support
+    SupportsArithmetic supportsArithmetic = () -> true;
+
+    default boolean supportsArithmetic() {
+        return supportsArithmetic.isSupported();
+    }
+
+    default void validateOperationSupport(String operation) {
+        // default: do nothing
+    }
+}
